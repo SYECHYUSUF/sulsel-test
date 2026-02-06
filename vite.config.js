f229e8bafd02709ab5276fdf34281c93741659ff
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-02-06 20:45:38 +0800

    publish api branch

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    server: {
+        host: "192.168.91.89", // Sesuaikan dengan IP yang Anda gunakan di PHP server
+        hmr: {
+            host: "192.168.91.89",
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/sidebar.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/css/sidebar.css",
+                "resources/js/app.js",
+            ],
             refresh: true,
         }),
     ],
