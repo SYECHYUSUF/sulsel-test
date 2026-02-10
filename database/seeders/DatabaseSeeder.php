56c2240509defbfd3e339cbfd59ed00e74dcbb32
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-02-10 14:48:44 +0800

    add opd database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -27,5 +29,19 @@ class DatabaseSeeder extends Seeder
             InformasiPublikSeeder::class,
             FaqSeeder::class,
         ]);
+
+        // --- Konfigurasi User Esdm 2 ---
+        // $esdmData = [
+        //     'id' => 6000,
+        //     'name' => 'Esdm 2',
+        //     'username' => 'esdm2',
+        //     'email' => 'esdm2@gmail.com',
+        //     'password' => Hash::make('esdm123'),
+        //     'id_skpd' => 'SKPD007',
+        // ];
+
+        // User::where('email', $esdmData['email'])->orWhere('username', $esdmData['username'])->delete();
+        // $userEsdm = User::create($esdmData);
+        // $userEsdm->addRole('opd');
     }
 }
