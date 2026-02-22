b242f07ec94d2eec7bddd7fcb20eb110b379e306
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-02-23 02:55:00 +0800

    fix routes and discard api/index

--- api/index.php
+++ api/index.php
@@ -1,10 +1,4 @@
 <?php
+// Forward request ke file index.php bawaan Laravel di folder public
 
-// Perbaiki URI agar tidak double /api atau mengandung /public
-$uri = $_SERVER['REQUEST_URI'];
-if (strpos($uri, '/api/api/') === 0) {
-    $_SERVER['REQUEST_URI'] = str_replace('/api/api/', '/api/', $uri);
-}
-
-// Load index Laravel dari folder public
 require __DIR__ . '/../public/index.php';
