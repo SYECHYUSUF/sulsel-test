67a293316e83bf7fe1c424f142b6070dea95d547
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-03-09 14:35:54 +0800

    fix change supabase storage

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,39 +11,39 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Seed roles first
-        // $this->call([
-        //     SqlFileSeeder::class,
-        //     InformasiSeeder::class,
+        $this->call([
+            SqlFileSeeder::class,
+            InformasiSeeder::class,
 
-        //     RoleSeeder::class,
-        //     FaqSeeder::class,
-        //     SurveySeeder::class,
-        //     SurveyResponseSeeder::class,
-        //     TahunSeeder::class,
-        //     SlideBannerSeeder::class,
-        //     MasterPekerjaanSeeder::class,
-        //     MasterDomisiliSeeder::class,
-        //     SosmedSeeder::class,
-        //     ProfilSeeder::class,
-        //     InformationSeeder::class,
-        //     FooterSettingSeeder::class,
-        //     InformasiPublikSeeder::class,
-        //     FaqSeeder::class,
-        // ]);
+            RoleSeeder::class,
+            FaqSeeder::class,
+            SurveySeeder::class,
+            SurveyResponseSeeder::class,
+            TahunSeeder::class,
+            SlideBannerSeeder::class,
+            MasterPekerjaanSeeder::class,
+            MasterDomisiliSeeder::class,
+            SosmedSeeder::class,
+            ProfilSeeder::class,
+            InformationSeeder::class,
+            FooterSettingSeeder::class,
+            InformasiPublikSeeder::class,
+            FaqSeeder::class,
+        ]);
 
         // --- Konfigurasi User Esdm 2 ---
-        $esdmData = [
-            'id' => 6000,
-            'name' => 'Esdm 2',
-            'username' => 'esdm2',
-            'email' => 'esdm2@gmail.com',
-            'password' => Hash::make('esdm123'),
-            'id_skpd' => 'SKPD007',
-        ];
+        // $esdmData = [
+        //     'id' => 6000,
+        //     'name' => 'Esdm 2',
+        //     'username' => 'esdm2',
+        //     'email' => 'esdm2@gmail.com',
+        //     'password' => Hash::make('esdm123'),
+        //     'id_skpd' => 'SKPD007',
+        // ];
 
-        User::where('email', $esdmData['email'])->orWhere('username', $esdmData['username'])->delete();
-        $userEsdm = User::create($esdmData);
-        $userEsdm->addRole('opd');
+        // User::where('email', $esdmData['email'])->orWhere('username', $esdmData['username'])->delete();
+        // $userEsdm = User::create($esdmData);
+        // $userEsdm->addRole('opd');
 
         // --- Konfigurasi User dpmd 2 ---
         // $esdmData = [
--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,21 @@ Route::get('/', function () {
 //  Serve files from the storage/app/public directory
 //  Example URL:
 //  http://localhost:8000/uploads/logo-skpd/VuwzepLEmFqOdrmDB4EMGmkx4i9S6NAoXo1cMVIV.png
-Route::get('/uploads/{path}', function ($path) {
-    return response()->file(
-        storage_path('app/public/' . $path)
-    );
-})->where('path', '.*');
+// Route::get('/uploads/{path}', function ($path) {
+//     return response()->file(
+//         storage_path('app/public/' . $path)
+//     );
+// })->where('path', '.*');
 
 // Redirect dari URL lokal ke URL publik Supabase
-// Route::get('/uploads/{path}', function ($path) {
-//     // Gunakan Project ID yang benar dari URL valid kamu
-//     $projectId = 'jlosrcqjysykztuhegqs';
-//     $bucket = env('SUPABASE_STORAGE_BUCKET', 'ppid-sulselprov-bucket');
+Route::get('/uploads/{path}', function ($path) {
+    // Gunakan Project ID yang benar dari URL valid kamu
+    $projectId = 'jlosrcqjysykztuhegqs';
+    $bucket = env('SUPABASE_STORAGE_BUCKET', 'ppid-sulselprov-bucket');
 
-//     // Susun URL sesuai format yang valid
-//     $publicUrl = "https://{$projectId}.supabase.co/storage/v1/object/public/{$bucket}/{$path}";
+    // Susun URL sesuai format yang valid
+    $publicUrl = "https://{$projectId}.supabase.co/storage/v1/object/public/{$bucket}/{$path}";
 
-//     // Gunakan away() untuk memastikan redirect ke domain eksternal secara penuh
-//     return Redirect::away($publicUrl);
-// })->where('path', '.*');
+    // Gunakan away() untuk memastikan redirect ke domain eksternal secara penuh
+    return Redirect::away($publicUrl);
+})->where('path', '.*');
