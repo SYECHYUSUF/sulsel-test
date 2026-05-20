30793123cea0676c7c76cc32ad288afd9ac02454
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-05-21 02:28:51 +0800

    comment out user configuration data in DatabaseSeeder for clarity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,31 +32,31 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // --- Konfigurasi User Esdm 2 ---
-        $esdmData = [
-            'id' => 6000,
-            'name' => 'Esdm 2',
-            'username' => 'esdm2',
-            'email' => 'esdm2@gmail.com',
-            'password' => Hash::make('esdm123'),
-            'id_skpd' => 'SKPD007',
-        ];
+        // $esdmData = [
+        //     'id' => 6000,
+        //     'name' => 'Esdm 2',
+        //     'username' => 'esdm2',
+        //     'email' => 'esdm2@gmail.com',
+        //     'password' => Hash::make('esdm123'),
+        //     'id_skpd' => 'SKPD007',
+        // ];
 
-        User::where('email', $esdmData['email'])->orWhere('username', $esdmData['username'])->delete();
-        $userEsdm = User::create($esdmData);
-        $userEsdm->addRole('opd');
+        // User::where('email', $esdmData['email'])->orWhere('username', $esdmData['username'])->delete();
+        // $userEsdm = User::create($esdmData);
+        // $userEsdm->addRole('opd');
 
-        // --- Konfigurasi User dpmd 2 ---
-        $esdmData = [
-            'id' => 6001,
-            'name' => 'DPMD 2',
-            'username' => 'dpmd2',
-            'email' => 'dpmd2@gmail.com',
-            'password' => Hash::make('dpmd123'),
-            'id_skpd' => 'SKPD006',
-        ];
+        // // --- Konfigurasi User dpmd 2 ---
+        // $esdmData = [
+        //     'id' => 6001,
+        //     'name' => 'DPMD 2',
+        //     'username' => 'dpmd2',
+        //     'email' => 'dpmd2@gmail.com',
+        //     'password' => Hash::make('dpmd123'),
+        //     'id_skpd' => 'SKPD006',
+        // ];
 
-        User::where('email', $esdmData['email'])->orWhere('username', $esdmData['username'])->delete();
-        $userEsdm = User::create($esdmData);
-        $userEsdm->addRole('opd');
+        // User::where('email', $esdmData['email'])->orWhere('username', $esdmData['username'])->delete();
+        // $userEsdm = User::create($esdmData);
+        // $userEsdm->addRole('opd');
     }
 }
